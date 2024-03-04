17dd7e3d4ba7882cd9c24edd5dc645f4b780ba96
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2024-03-04 18:53:56 +0900

    [level 2] Title: 진료과별 총 예약 횟수 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/2/132202. 진료과별 총 예약 횟수 출력하기/진료과별 총 예약 횟수 출력하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT MCDP_CD as '진료과 코드', count(*) as '5월예약건수'
+from appointment
+where apnt_ymd between '2022-05-01' and '2022-05-31'
+group by MCDP_CD
+order by count(*) asc, mcdp_cd asc
+
+
