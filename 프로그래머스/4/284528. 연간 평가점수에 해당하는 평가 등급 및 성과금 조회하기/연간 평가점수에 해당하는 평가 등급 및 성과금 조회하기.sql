63c0f591b41d90b9dd3e23a12bae025b4c15ec9a
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2024-09-14 13:31:46 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,17 @@
+-- 코드를 작성해주세요
+SELECT
+    e.EMP_NO,
+    e.EMP_NAME,
+    CASE WHEN AVG(g.SCORE) >= 96 THEN 'S' 
+         WHEN AVG(g.SCORE) >= 90 THEN 'A'
+         WHEN AVG(g.SCORE) >= 80 THEN 'B'
+         ELSE 'C' END as GRADE,
+    CASE WHEN AVG(g.SCORE) >= 96 THEN SAL * 0.2
+         WHEN AVG(g.SCORE) >= 90 THEN SAL * 0.15
+         WHEN AVG(g.SCORE) >= 80 THEN SAL * 0.1
+         ELSE 0 END as BONUS
+FROM HR_EMPLOYEES e
+JOIN HR_DEPARTMENT d on d.DEPT_ID = e.DEPT_ID
+JOIN HR_GRADE g on g.EMP_NO = e.EMP_NO
+GROUP BY 1
+ORDER BY 1 ASC
