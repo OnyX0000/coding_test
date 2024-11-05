9eb3be7500a1de578160a90bfb8e7e0e821f42c0
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2024-11-05 16:26:32 +0900

    Time: 612 ms (54.86%), Space: 0B (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -3,16 +3,24 @@ WITH T1 as
 (
     SELECT * 
     FROM Trips
-    WHERE client_id not in (SELECT users_id FROM Users WHERE role = 'client' and banned = 'Yes')
+    WHERE client_id NOT IN (
+        SELECT users_id 
+        FROM Users 
+        WHERE role = 'client' 
+            and banned = 'Yes')
 ),
 T2 as (
     SELECT *
     FROM T1
-    WHERE driver_id not in (SELECT users_id FROM Users WHERE role = 'driver' and banned = 'Yes')
+    WHERE driver_id NOT IN (
+        SELECT users_id 
+        FROM Users 
+        WHERE role = 'driver' 
+            and banned = 'Yes')
 )
 SELECT 
     request_at as Day,
-    ROUND(SUM(CASE WHEN status != 'completed' THEN 1 ELSE 0 END) / COUNT(id), 2) AS 'Cancellation Rate'
+    ROUND(SUM(CASE WHEN status != 'completed' THEN 1 ELSE 0 END) / COUNT(id), 2) as 'Cancellation Rate'
 FROM T2 
 WHERE request_at between '2013-10-01' and '2013-10-03'
 GROUP BY 1
