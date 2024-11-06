2ef417814171f275a292b5648016891d3b2c3978
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2024-11-06 17:49:34 +0900

    Time: 564 ms (84.79%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,10 +1,12 @@
 # Write your MySQL query statement below
 SELECT 
     ROUND(COUNT(DISTINCT a.player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) as fraction
-FROM Activity  a
-INNER JOIN
-    (SELECT player_id, MIN(event_date) AS min_event_date 
+FROM Activity a
+INNER JOIN (
+    SELECT 
+        player_id, 
+        MIN(event_date) as min_event_date 
     FROM Activity 
-    GROUP BY 1) as b
-ON a.player_id = b.player_id
+    GROUP BY 1
+) as b on a.player_id = b.player_id
 WHERE DATEDIFF(a.event_date, b.min_event_date) = 1
