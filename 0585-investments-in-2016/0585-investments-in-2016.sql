2a9bb6202b774147305ebaaacfee374b7f51021a
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2024-11-08 11:07:36 +0900

    Time: 848 ms (11.89%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,15 +1,16 @@
 # Write your MySQL query statement below
-SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+SELECT 
+    ROUND(SUM(tiv_2016), 2) as tiv_2016
 FROM Insurance
-WHERE tiv_2015 IN (
+WHERE tiv_2015 in (
     SELECT tiv_2015
     FROM Insurance
-    GROUP BY tiv_2015
+    GROUP BY 1
     HAVING COUNT(*) > 1
 )
-AND (lat, lon) IN (
+and (lat, lon) in (
     SELECT lat, lon
     FROM Insurance
-    GROUP BY lat, lon
+    GROUP BY 1, 2
     HAVING COUNT(*) = 1
 )
