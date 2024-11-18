c087ab969b77b9bf6878607fe8627796b577f1dd
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2024-11-18 12:41:49 +0900

    Time: 478 ms (88.39%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -3,5 +3,5 @@ SELECT
     activity_date as day, 
     COUNT(DISTINCT user_id) as active_users
 FROM Activity
-WHERE (DATEDIFF("2019-7-27", activity_date) <= 30)
-GROUP BY activity_date
+WHERE activity_date between '2019-06-28' and '2019-07-27'
+GROUP BY 1
