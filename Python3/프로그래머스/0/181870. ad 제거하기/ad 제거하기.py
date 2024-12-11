58116daad1a219d2e331527f22e87639ddb1e5cb
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2024-12-11 13:25:06 +0900

    [level 0] Title: ad 제거하기, Time: 1.54 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -0,0 +1,9 @@
+def solution(strArr):
+    answer = []
+    for i in strArr[:] :
+        if 'ad' in i :
+            strArr.remove(i)
+        else : 
+            continue
+    answer = strArr
+    return answer
