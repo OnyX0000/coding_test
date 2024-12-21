ebd2e573f493ea464e5cb3250177bfd6af0f8028
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2024-12-21 20:31:57 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,9 @@
+def solution(numer1, denom1, numer2, denom2):
+    numerator = numer1 * denom2 + numer2 * denom1
+    denominator = denom1 * denom2
+
+    a, b = numerator, denominator
+    while b:
+        a, b = b, a % b
+
+    return [numerator // a, denominator // a]
