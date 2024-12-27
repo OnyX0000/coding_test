780b675ecdeb7fbcf27bf8f94f3aab843916f158
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2024-12-27 23:55:13 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.48 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    sums = set()
+    
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)): 
+            sums.add(numbers[i] + numbers[j])  
+            
+    return sorted(sums)
