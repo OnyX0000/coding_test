240695234cbf07ed2171de9f3842878cb0f4662e
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2024-12-30 22:19:03 +0900

    [level 1] Title: 콜라 문제, Time: 0.61 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -0,0 +1,14 @@
+def solution(a, b, n):
+    answer = 0
+
+    while n >= a :
+        # 교환 가능한 콜라의 병 수
+        exchanged = (n // a) * b
+        # 남은 빈 병 수
+        remaining = n % a
+        # 총 병 수 갱신
+        n = exchanged + remaining
+        # 받은 콜라 누적
+        answer += exchanged
+
+    return answer
