371f28ec28a1a79a8bb98ff4777ac7081fdb206f
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2025-01-02 23:21:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 72.49 ms, Memory: 26.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -10,3 +10,21 @@ def solution(participant, completion):
     
     # 마지막 남은 선수가 완주하지 못한 선수
     return participant[-1]
+
+# def solution(participant, completion):
+#     # 해시 값을 이용한 합산
+#     hash_sum = 0
+#     hash_dict = {}
+    
+#     # 참가자 해시값을 더하고 해시 테이블에 기록
+#     for person in participant :
+#         hash_val = hash(person)
+#         hash_sum += hash_val
+#         hash_dict[hash_val] = person
+
+#     # 완주한 사람의 해시값을 빼기
+#     for person in completion :
+#         hash_sum -= hash(person)
+
+#     # 남은 해시값으로 완주하지 못한 사람 찾기
+#     return hash_dict[hash_sum]
