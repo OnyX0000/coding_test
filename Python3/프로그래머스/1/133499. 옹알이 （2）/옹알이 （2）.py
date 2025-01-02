ce971822ff3bda7ead8d100fe2f558d7f429c207
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2025-01-02 12:54:26 +0900

    [level 1] Title: 옹알이 (2), Time: 0.11 ms, Memory: 10 MB -BaekjoonHub

--- Python3/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ Python3/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,13 +1,3 @@
 def solution(babbling):
-    valid_sounds = ["aya", "ye", "woo", "ma"]
-    count = 0
-
-    for word in babbling :
-        for sound in valid_sounds :
-            if sound * 2 in word :  
-                break
-            word = word.replace(sound, " ")  
-        if word.strip() == "" : 
-            count += 1
-
-    return count
+    answer = 0
+    return answer
