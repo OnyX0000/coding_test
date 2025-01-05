81701949a85cc9b9b3b3322e64f283b667e2e2fd
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2025-01-05 16:36:33 +0900

    [level 1] Title: 폰켓몬, Time: 0.76 ms, Memory: 11 MB -BaekjoonHub

--- Python3/프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ Python3/프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,3 +1,3 @@
 def solution(nums):
-    # 선택할 수 있는 폰켓몬 종류의 수는 N/2 또는 고유한 종류 수 중 작은 값
+    # 선택할 수 있는 폰켓몬 종류의 수는 N/2(선택할 수 있는 최대 수) 또는 고유한 종류 수 중 작은 값
     return min(len(set(nums)), len(nums) // 2)
