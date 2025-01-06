4946e578c81200299fe906667180818d4a83c5d6
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2025-01-06 13:10:20 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ Python3/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -4,10 +4,10 @@ def solution(s):
     
     for i in temp :
         i = i.capitalize()
-        if answer == '' :
+        if answer == '' :   # 처음에는 공백을 넣지 않음
             answer += i
         else :
-            answer += ' '
+            answer += ' '   # 처음이 아니라면 문자를 더하기 전에 공백을 추가 
             answer += i
         
     return answer
