2a042f293d7403c59175ef5c741beacdf621f02e
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2025-01-11 20:11:30 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ Python3/프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,11 +1,2 @@
 def solution(n):
-    battery = 0
-    
-    while n > 0 :
-        if n % 2 == 0 :
-            n //= 2
-        else :
-            n -= 1
-            battery += 1
-            
-    return battery
+    return bin(n).count('1')
