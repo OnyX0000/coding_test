a0de9e659e0a8aae8a4cc9863ffde9561dc42046
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2025-01-14 11:13:43 +0900

    [level 2] Title: n^2 배열 자르기, Time: 695.65 ms, Memory: 430 MB -BaekjoonHub

--- Python3/프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ Python3/프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -8,3 +8,9 @@ def solution(n, left, right):
         a += ([j] * j + list(range(j + 1, n + 1)))
 
     return a[lc:(rr - lr) * n + rc + 1]    
+
+# def solution(n, left, right):
+#     answer = []
+#     for i in range(left,right+1):
+#         answer.append(max(i//n,i%n)+1)
+#     return answer
