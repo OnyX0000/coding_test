b7035ebd8ee880c1f99414efaaa5e638a9525b1b
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2025-01-15 11:32:39 +0900

    [level 2] Title: H-Index, Time: 0.65 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,8 @@
+def solution(citations):
+    result = 0
+    citations.sort(reverse = True)
+    
+    for idx, i in enumerate(citations, start = 1) :
+        result = max(result, min(i, idx))
+        
+    return result 
