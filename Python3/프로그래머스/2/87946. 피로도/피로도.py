44317d7c16e82719e95430032fc41a9acdc23c49
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2025-01-16 17:07:31 +0900

    [level 2] Title: 피로도, Time: 94.15 ms, Memory: 15.5 MB -BaekjoonHub

--- Python3/프로그래머스/2/87946. 피로도/피로도.py
+++ Python3/프로그래머스/2/87946. 피로도/피로도.py
@@ -1,3 +1,31 @@
 def solution(k, dungeons):
-    answer = -1
-    return answer
+    def generate_permutations(arr, depth, n, result, current):
+        if depth == n:
+            result.append(current[:])
+            return
+        for i in range(len(arr)):
+            generate_permutations(arr[:i] + arr[i+1:], depth + 1, n, result, current + [arr[i]])
+
+    max_explored = 0
+    n = len(dungeons)
+    permutations = []
+
+    # 순열 생성
+    generate_permutations(dungeons, 0, n, permutations, [])
+
+    # 생성된 순열로 탐험 시뮬레이션
+    for order in permutations:
+        current_fatigue = k
+        explored_count = 0
+
+        for required, consume in order:
+            if current_fatigue >= required:
+                current_fatigue -= consume
+                explored_count += 1
+            else:
+                break
+
+        # 최대 탐험 가능한 던전 수 갱신
+        max_explored = max(max_explored, explored_count)
+
+    return max_explored
