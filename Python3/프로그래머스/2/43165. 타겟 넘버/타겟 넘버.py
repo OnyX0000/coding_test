cb37b4c34a30faa0983dc7bfef81b06078f17cf9
Lee Jinkyu <jinkyu2jinkyu@gmail.com>
2025-02-02 19:25:56 +0900

    [level 2] Title: 타겟 넘버, Time: 378.29 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -4,8 +4,8 @@ def solution(numbers, target):
     
     def dfs(i, total) :
         global answer
-        if (i == len(numbers)) :
-            if total == target :
+        if (i == len(numbers)) :  # numbers 배열을 끝까지 탐색한 경우
+            if total == target :  # 목표값과 같은 경우 answer 증가
                 answer += 1
             return
         dfs(i + 1, total + numbers[i])    
